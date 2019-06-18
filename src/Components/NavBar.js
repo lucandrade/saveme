4b66b7f454ddeaef39fde5796aef010aa473347e
Lucas Andrade de Oliveira <lucasandradedeveloper@gmail.com>
2019-06-18 18:25:47 +0100

    adding dropdown

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,38 @@
+import Dropdown from 'rc-dropdown';
+import 'rc-dropdown/assets/index.css';
 import React, { Component } from 'react';
 
 export default class NavBar extends Component {
+    renderMenuItems() {
+        return (
+            <ul>
+                <li>
+                    Clear Notes
+                </li>
+            </ul>
+        );
+    }
+
+    renderMenu() {
+        return (
+            <Dropdown trigger={['click']}
+                overlayClassName="sm-dropdown"
+                overlay={this.renderMenuItems()}>
+                <div className="sm-dropdown-button">
+                    <i className="fas fa-bars" />
+                </div>
+            </Dropdown>
+        );
+    }
+    
     render() {
         return (
             <nav className="sm-nav">
                 <div className="sm-title">
                     <h1>Save Me</h1>
+                </div>
+                <div className="sm-actions">
+                    {this.renderMenu()}
                 </div>
             </nav>
         );
